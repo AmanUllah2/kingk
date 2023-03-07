50b974c205ece0c8925cdd5459a542c2079b1540
Aman Ullah <amansheikh121@gmail.com>
2023-03-07 12:23:32 +0500

    repo change

--- apps/bridge/components/layerZero/AptosBridgeFooter.tsx
+++ apps/bridge/components/layerZero/AptosBridgeFooter.tsx
@@ -1,16 +1,16 @@
-import PoweredBy from 'components/layerZero/PoweredBy'
-import { LinkExternal } from '@pancakeswap/uikit'
+// import PoweredBy from 'components/layerZero/PoweredBy'
+// import { LinkExternal } from '@pancakeswap/uikit'
 
 const AptosBridgeFooter = () => {
   return (
     <>
-      <PoweredBy />
+      {/* <PoweredBy />
       <LinkExternal m="20px auto" href="https://docs.pancakeswap.finance/get-started-aptos/aptos-faq#cake-bridging">
         Need Help?
       </LinkExternal>
       <LinkExternal m="20px auto" href="https://docs.pancakeswap.finance/get-started-aptos/aptos-coin-guide">
         Don’t see your assets?
-      </LinkExternal>
+      </LinkExternal> */}
     </>
   )
 }
